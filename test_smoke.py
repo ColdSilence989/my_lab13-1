6eb978f6893c17b43ba3927f2d9670b23800d539
Oleg Pizhenko <102592493+ColdSilence989@users.noreply.github.com>
2026-01-17 19:12:22 +0300

    Delete test_smoke.py

--- test_smoke.py
+++
@@ -1,9 +0,0 @@
-# test_smoke.py
-def test_math():
-    assert 1 + 1 == 2
-
-def test_imports():
-    try:
-        import app
-    except ImportError:
-        pass # Если упадет из-за зависимостей, хоть тест пройдет
